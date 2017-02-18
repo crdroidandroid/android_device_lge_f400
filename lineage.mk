07d00a66e8f014ce0ec712ecd907070fcd696e6c
Joonhyuk Song <fprhqkrtk303@naver.com>
2017-02-18 15:28:47 +0900

    f400: update fingerprint and description from 30g
    Change-Id: Ida2962ee18f04751c84cd2aa05925128cfe10608

--- lineage.mk
+++ lineage.mk
@@ -5,4 +5,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_f400
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_kt_kr" BUILD_FINGERPRINT="lge/g3_kt_kr/g3:5.0/LRX21R/F400K20f.1419232865:user/release-keys" PRIVATE_BUILD_DESC="g3_kt_kr-user 5.0 LRX21R F400K20f.1419232865 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_kt_kr" \
+    BUILD_FINGERPRINT="lge/g3_kt_kr/g3:6.0/MRA58K/1606909301116:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_kt_kr-user 6.0 MRA58K 1606909301116 release-keys"
