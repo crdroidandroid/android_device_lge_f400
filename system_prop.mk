44bdf281cee02c24d240b4d2bf177bedc8180e30
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-02-15 14:08:38 +0100

    f400: Include system_prop.mk
     * Fixes RIL not recognising SIM card
     * Fixes a mismerge in 06d7c3a03705b4e3a9b83c98f5867d10847b29cb

--- system_prop.mk
+++ system_prop.mk
@@ -4,6 +4,6 @@
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
-telephony.lteOnGsmDevice=0 \
-ro.telephony.default_network=10
+    rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
+    telephony.lteOnGsmDevice=0 \
+    ro.telephony.default_network=10
